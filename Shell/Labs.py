6612b72a1c34028f297a7062fa9355c3f73e6323
Lena <elvslv@gmail.com>
2012-03-01 22:51:21 +1100

    add loading and storing to file

--- Shell/Labs.py
+++ Shell/Labs.py
@@ -2,7 +2,10 @@
 
 from PyQt4 import QtGui, QtCore
 import random, math
+import os.path
+import os
 from threading import Thread
+from Misc import *
 
 class Labs_(QtGui.QWidget):
 	def __init__(self, parent, num):
@@ -30,6 +33,20 @@ class labThread(Thread):
 		self.lab.expProb.setText(str(result))
 		self.lab.eps.setText(str(abs(result - self.lab.defaultValues[self.task - 1][1])))
 		self.lab.parent.changeState('')
+
+class labThread2(Thread):
+	def __init__ (self, lab):
+		Thread.__init__(self)
+		self.expNum = lab.expNum.value()
+		self.lab = lab
+
+	def run(self):
+		self.lab.parent.changeState(u'Выполняется...')
+		cnt = 0
+		for i in range(self.expNum):
+			self.lab.sample.append(random.randint(0, 999))
+		self.lab.parent.changeState('')
+		self.lab.generatedSignal.emit()
 
 
 class Lab1(Labs_):
@@ -171,4 +188,135 @@ class Lab1(Labs_):
 		r = self.segmentLength.value() + 0.0
 		return 4 * (r * r - c * c) <= r * r
 		
+class Lab2(Labs_):
+	generatedSignal = QtCore.pyqtSignal()
+	def __init__(self, parent):
+		super(Lab2, self).__init__(parent, 1)
+		self.parent = parent
+
+		self.verticalLayout = QtGui.QVBoxLayout(self)
+		self.setLayout(self.verticalLayout)
+
+		self.taskDescription = QtGui.QTextBrowser(self)
+		self.taskDescription.setSource(QtCore.QUrl("TaskDescription2.html"))
+		self.verticalLayout.addWidget(self.taskDescription)
+
+		self.subtasksComboBox = QtGui.QComboBox(self)
+		self.verticalLayout.addWidget(self.subtasksComboBox)
 		
+		items = [u'Выберите подзадачу', u'1', u'2']
+		answers = [0.504, 0.432, 0.036, 0.027]
+		for item in items:
+			self.subtasksComboBox.addItem(item)
+			
+		self.subtasksComboBox.currentIndexChanged.connect(self.comboboxChanged)
+
+		layout = QtGui.QVBoxLayout(self)
+		self.verticalLayout.addLayout(layout)
+		
+		self.solLayout = QtGui.QGridLayout(self)
+		layout.setSizeConstraint(QtGui.QLayout.SetMaximumSize)
+		layout.addLayout(self.solLayout)
+		
+		label = QtGui.QLabel(u'Источник выборки')
+		self.solLayout.addWidget(label, 0, 0)
+		self.source = QtGui.QComboBox(self)
+		self.solLayout.addWidget(self.source, 0, 1)
+		self.source.addItems([u'Сгенерировать выборку', u'Загрузить из файла'])
+		
+		label = QtGui.QLabel(u'Количество элементов выборки')
+		self.solLayout.addWidget(label, 1, 0)
+		self.expNum = QtGui.QSpinBox(self)
+		self.solLayout.addWidget(self.expNum, 1, 1)
+		self.expNum.setRange(0, 1000000000)
+		self.expNum.setValue(1000000)
+		
+		self.gen = [[label, self.expNum]]
+		
+		label = QtGui.QLabel(u'Не сохранять в файл')
+		self.solLayout.addWidget(label, 2, 0)
+		self.dontSave = QtGui.QCheckBox(self)
+		self.solLayout.addWidget(self.dontSave, 2, 1)
+		btn = QtGui.QPushButton(self)
+		btn.setText(u'Сгенерировать выборку')
+		btn.clicked.connect(self.generate)
+		self.solLayout.addWidget(btn, 3, 0)
+		self.isGeneratedLabel = QtGui.QLabel(u'Выборка не сгенерирована')
+		self.solLayout.addWidget(self.isGeneratedLabel, 3, 1)
+		self.isGenerated = False
+		self.gen[0].extend([label, self.dontSave, btn, self.isGeneratedLabel])	
+
+		self.selectFile = QtGui.QPushButton(self)
+		self.selectFile.setText(u'Выбрать файл')
+		self.solLayout.addWidget(self.selectFile, 4, 0)
+		self.selectFile.clicked.connect(self.selectFilePressed)
+		self.calc = QtGui.QPushButton(self)
+		self.calc.setText(u'Анализировать выборку')
+		self.calc.clicked.connect(self.count)
+		self.solLayout.addWidget(self.calc, 5, 0)
+		self.generatedSignal.connect(self.generated)
+		#self.changeControlsVisibility(False)
+
+		#self.defaultValues = [[100000, 0.504], [100000, 0.432], [100000, 0.036], [100000, 0.027],  [100000, 1 - math.sqrt(3) / 2]]
+
+	def selectFilePressed(self):
+		fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file',  os.getcwd())
+		if os.path.isfile(fname):
+			self.isGeneratedLabel.setText(u'Загружается выборка...')
+			self.isGenerated = False
+			self.sample = []
+			try:
+				f = open(fname, 'r')
+				for i, val in enumerate(f.read().split()):
+					v = int(val)
+					assert v > 0 if not i else v >= 0 and v <= 999
+					if not i:
+						self.expNum.setValue(v)
+					else:
+						self.sample.append(v)
+				f.close()
+				self.isGeneratedLabel.setText(u'Выборка загружена')
+			except (Exception, e):
+				self.isGeneratedLabel.setText(u'Выборка не сгенерирована')
+				showMessage(u'Ошибка', u'Некорректный формат файла')
+
+	def generated(self):
+		self.isGenerated = True
+		self.isGeneratedLabel.setText(u'Выборка сгенерирована')
+		if not self.dontSave.isChecked():
+			f = open('result.txt', 'w')
+			f.write('%s ' %  self.expNum.value())
+			for v in self.sample:
+				f.write('%s ' %  v)
+			f.close()
+
+	def generate(self):
+		self.isGeneratedLabel.setText(u'Выборка не сгенерирована')
+		self.isGenerated = False
+		self.sample = []
+		thread = labThread2(self)
+		thread.start()
+
+	def changeControlsVisibility(self, visible):
+		for i in range(self.solLayout.count()):
+			self.solLayout.itemAt(i).widget().setVisible(visible)
+
+	def count(self):
+		task = self.subtasksComboBox.currentIndex()
+		thread = labThread(self, task)
+		thread.start()
+	
+	@QtCore.pyqtSlot(int)
+	def comboboxChanged(self, index):
+		if not index:
+			self.changeControlsVisibility(False)
+			return
+
+		self.expNum.setValue(self.defaultValues[index - 1][0])
+		self.theorProb.setText(str(self.defaultValues[index - 1][1]))
+
+		self.changeControlsVisibility(True)
+		self.segmentLength.setVisible(index not in range(1, 5))
+		self.segmentLengthLabel.setVisible(index not in range(1, 5))
+		self.count()
+
--- Shell/Misc.py
+++ Shell/Misc.py
@@ -1,5 +1,7 @@
+from PyQt4 import QtGui, QtCore
+
 global NUM_OF_LABS;
-NUM_OF_LABS = 1;
+NUM_OF_LABS = 2;
 
 def showMessage(title, message):
 	mbox = QtGui.QMessageBox(QtGui.QMessageBox.Critical, title,
