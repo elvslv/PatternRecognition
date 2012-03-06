3ba8a0565d9a7bf18d8ba7a2fddb0deb4c64d6ac
Lena <elvslv@gmail.com>
2012-03-06 21:15:32 +1100

    add second part of second lab

--- Shell/Labs2.py
+++ Shell/Labs2.py
@@ -11,6 +11,33 @@ from Labs import *
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import pylab
+import numpy as np
+
+class ECDF:
+	def __init__(self, observations):
+		"Parameters: observations is a NumPy array."
+		self.observations = np.array(observations)
+
+	def __call__(self, x): 
+		try:
+			return (self.observations <= x).mean()
+		except AttributeError:
+			# self.observations probably needs to be converted to
+			# a NumPy array
+			self.observations = np.array(self.observations)
+			return (self.observations <= x).mean()
+
+	def count(self, a=None, b=None, num = 100): 
+		if a == None:
+			# Set up a reasonable default
+			a = self.observations.min() - self.observations.std()
+		if b == None:
+			# Set up a reasonable default
+			b = self.observations.max() + self.observations.std()
+		X = np.linspace(a, b, num)
+		f = np.vectorize(self.__call__)
+		return X, f(X)
+
 class MyMplCanvas(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=100):
 		fig = Figure(figsize=(width, height), dpi=dpi)
@@ -30,8 +57,8 @@ class MyStaticMplCanvas(MyMplCanvas):
 		self.axes.plot(x, y)
 		self.draw()
 		
-	def hist(self, x):
-		self.axes.hist(x, bins = 28)
+	def hist(self, x, b = 28):
+		self.axes.hist(x, bins = b)
 		self.draw()
 
 class labThread2(Thread):
@@ -55,14 +82,13 @@ class Lab2(Labs_):
 		
 		tabWidget = QtGui.QTabWidget(self)
 		self.verticalLayout.addWidget(tabWidget)
-		tabWidget.addTab(self.sc1, u'Многоугольник')
+		tabWidget.addTab(self.sc1, u'Функция распределения')
 		tabWidget.addTab(self.sc2, u'Гистограмма')
-		#self.verticalLayout.addWidget(self.sc)
 		
 		self.subtasksComboBox = QtGui.QComboBox(self)
 		self.verticalLayout.addWidget(self.subtasksComboBox)
 		
-		items = [u'Выберите подзадачу', u'1', u'2']
+		items = [u'1', u'2']
 		for item in items:
 			self.subtasksComboBox.addItem(item)
 			
@@ -116,29 +142,29 @@ class Lab2(Labs_):
 		self.generatedSignal.connect(self.generated)
 		self.analyzedSignal.connect(self.analyzed)
 		
-		self.answers = [13.5, 24.75, 0, -0.40808]
+		self.answers = [[13.5, 24.75, 0, -0.40808], [0, 0.5, 0, 0]]
 		
 		self.results = [[] for i in range(4)]
 		
-		labels1 = [u'Теоретический результат', u'Экспериментальный результат', u'Разница']
-		labels2 = [u'Мат. ожидание', u'Дисперсия', u'Асимметрия', u'Эксцесс']
+		self.labels1 = [u'Теоретический результат', u'Экспериментальный результат', u'Разница']
+		self.labels2 = [u'Мат. ожидание', u'Дисперсия', u'Асимметрия', u'Эксцесс']
 		
-		for i, l in enumerate(labels1):
+		for i, l in enumerate(self.labels1):
 			label = QtGui.QLabel(l)
 			self.solLayout.addWidget(label, 0, 3 + i)
 			
-		for i, l in enumerate(labels2):
+		for i, l in enumerate(self.labels2):
 			label = QtGui.QLabel(l)
 			self.solLayout.addWidget(label, 1 + i, 2)
 			
-		for i in range(len(labels2)):
+		for i in range(len(self.labels2)):
 			self.results.append([])
-			for j in range(len(labels1)):
+			for j in range(len(self.labels1)):
 				self.results[i].append(QtGui.QLineEdit(self))
 				self.results[i][j].setReadOnly(True)
 				self.solLayout.addWidget(self.results[i][j], 1 + i, 3 + j)
 				if not j:
-					self.results[i][j].setText(str(self.answers[i]))
+					self.results[i][j].setText(str(self.answers[0][i]))
 
 		self.changeControlsVisibility()
 
@@ -177,53 +203,86 @@ class Lab2(Labs_):
 	def analyzed(self):
 		self.parent.changeState('')
 			
-	def startGenerate(self):
+	def startGenerateDiscrete(self):
 		N = self.expNum.value()
 		for i in range(N):
-			self.sample.append(random.randint(0, 999))
+			v = random.randint(0, 999)
+			self.sample.append((v / 100) + ((v / 10) % 10) + (v % 10))
+		self.generatedSignal.emit()		
+
+	def distributionDensity(self, x):
+		return pow(math.e, -x * x) / (math.sqrt(math.pi))
+	
+	def distributionFunction(self, x):
+		return (1 + math.erf(x)) / (2 + 0.0)
+	
+	def startGenerateContinious(self):
+		N = self.expNum.value()
+		cnt = 0
+		a = -10 #-2.185124219
+		b = 10#2.185124219
+		f_max = 1 / math.sqrt(math.pi)
+		
+		while(cnt < N):
+			x1 = random.random()
+			x2 = random.random()
+			y1 = a + (b - a) * x1
+			y2 = f_max * x2
+			if y2 <= self.distributionDensity(y1):
+				self.sample.append(y1)
+				cnt += 1
+				
 		self.generatedSignal.emit()			
-			
-	def startAnalyze(self):
+		
+	def countStatParams(self, task):
 		ans = [0 for i in range(4)] #m, D, gamma, k
 		N = len(self.sample)
-		self.sample1 = []
 		self.graph = [0 for i in range(28)]
 		for v in self.sample:
-			val = (v / 100) + ((v / 10) % 10) + (v % 10)
-			self.graph[val] += 1
-			ans[0] += val
-			self.sample1.append(val)
-			
+			if not task:
+				self.graph[v] += 1
+			ans[0] += v
 		ans[0] /= (N + 0.0)
 		for v in self.sample:
-			val = (v / 100) + ((v / 10) % 10) + (v % 10)
-			ans[1] += math.pow(val - ans[0],  2)
-			ans[2] += math.pow(val - ans[0],  3)
-			ans[3] += math.pow(val - ans[0],  4)
+			ans[1] += math.pow(v - ans[0],  2)
+			ans[2] += math.pow(v - ans[0],  3)
+			ans[3] += math.pow(v - ans[0],  4)
 		ans[1] /= (N + 0.0)
-		ans[2] /= N * math.pow(ans[1], 3 / 2)
+		ans[2] /= N * math.pow(ans[1], (3 + 0.0) / 2)
 		ans[3] /= N * math.pow(ans[1], 2)
 		ans[3] -= 3
 		for i in range(4):
 			self.results[i][1].setText(str(ans[i]))
-			self.results[i][2].setText(str(abs(ans[i] - self.answers[i])))
+			self.results[i][2].setText(str(abs(ans[i] - self.answers[task][i])))
+		return self.graph
+		
+	def startAnalyzeDescrete(self):
+		self.graph = self.countStatParams(0)
 		sum = 0
 		res = [0 for i in range(28)]
 		for i in range(28):
 			sum += (self.graph[i] + 0.0) / N
 			res[i] = sum
-		#self.graph = sorted(self.graph, key=lambda g: g[0]) 
-		self.sc2.hist(self.sample1)
+		self.sc2.hist(self.sample)
 		self.sc1.plot([i for i in range(28)], res)	
 					
 		self.analyzedSignal.emit()			
-			
+
+	def startAnalyzeContinious(self):
+		self.countStatParams(1)
+		self.sc2.hist(self.sample, 100) #must be optional!!!
+		F = ECDF(self.sample)
+		x, f = F.count()
+		self.sc1.plot(x, f)	
+					
+		self.analyzedSignal.emit()		
+		
 	def generate(self):
 		self.isGeneratedLabel.setText(u'Выборка не сгенерирована')
 		self.isGenerated = False
 		self.sample = []
 		self.parent.changeState(u'Выполняется...')
-		thread = labThread2(self, self.startGenerate)
+		thread = labThread2(self, self.startGenerateDiscrete if not self.subtasksComboBox.currentIndex() else self.startGenerateContinious)
 		thread.start()
 
 	def changeControlsVisibility(self):
@@ -231,26 +290,19 @@ class Lab2(Labs_):
 		for item in self.gen[0]:
 			item.setVisible(not i)
 		self.selectFile.setVisible(i)
+		
 		#for i in range(self.solLayout.count()):
 		#	self.solLayout.itemAt(i).widget().setVisible(visible)
 
 	def count(self):
 		self.parent.changeState(u'Анализируется...')
-		thread = labThread2(self, self.startAnalyze)
+		thread = labThread2(self, self.startAnalyzeDescrete if not self.subtasksComboBox.currentIndex() else self.startAnalyzeContinious)
 		thread.start()
 
 
 	@QtCore.pyqtSlot(int)
 	def comboboxChanged(self, index):
-		if not index:
-			self.changeControlsVisibility()
-			return
-
-		self.expNum.setValue(self.defaultValues[index - 1][0])
-		self.theorProb.setText(str(self.defaultValues[index - 1][1]))
-
-		self.changeControlsVisibility()
-		self.segmentLength.setVisible(index not in range(1, 5))
-		self.segmentLengthLabel.setVisible(index not in range(1, 5))
-		self.count()
-
+		task = self.subtasksComboBox.currentIndex()
+		for i in range(len(self.labels2)):
+			for j in range(len(self.labels1)):
+					self.results[i][j].setText(str(self.answers[task][i] if not j else 0))
